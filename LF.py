33bc3f50784f42607146f42d0ebd65f45df649d1
OpsssDess <OpsssDess@icloud.com>
2022-07-14 18:43:22 +0300

    добавил шаблоны

--- LF.py
+++ LF.py
@@ -1,37 +1,13 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 import json
+import jinja2
 
 app = Flask(__name__)
 stock = []
 
 @app.route('/')
 def main_page():
-    return """<html>
-                    <body>
-                        <section>
-                            <h2>Регистрация прошения</h2>
-                                <p>
-                                    <form action="/request/take" method="get">
-                                    <button type="submit">Взять.</button>
-                                    </form>
-                                </p>
-                        </section>
-                        <section>
-                            <h2>Регистрация пожертвования</h2>
-                                <p>
-                                    <form action="/request/donation" method="post">
-                                    <label for="name">Какую вещь хотите отдать?</label>
-                                    <input type="text" name="name">
-                                    <label for="name">Сколько штук?</label>
-                                    <input type="text" name="amount">
-                                    <label for="name">Куда хотите положить (FIFO or LIFO?</label>
-                                    <input type="text" name="name_stock">
-                                    <button type="submit">Отправить</button>
-                                    </form>
-                                </p>
-                        </section>
-                    </body>
-                </html>"""
+    return render_template('main_page.html')
 
 
 @app.route("/request/donation", methods=["POST"])
@@ -42,15 +18,7 @@ def donate():
         stock.insert(0,{"name": request.form['name'], "amount": int(request.form['amount'])})
     with open('good', 'w', encoding='utf-8') as f:
         json.dump(stock, f)
-    return f"""<html>
-                    <body>
-                        <section>
-                            <h2>Спасибо!!!</h2>
-                                <a href="{url_for('main_page')}">Вернуться на главную</a>
-                        </section>
-                    </body>
-                </html>"""
-
+    return render_template('request_donation.html')
 
 @app.route("/request/take", methods=["GET"])
 def ask_good():
@@ -68,25 +36,9 @@ def ask_good():
         with open('good', 'w', encoding='utf-8') as f:
             json.dump(stock1, f)
 
-        return f"""<html>
-                        <body>
-                            <section>
-                                <h2>Подойдите к окну</h2>
-                                    <p>Возьмите {donation['name']}, в количестве 1</p>
-                                    <a href="{url_for('main_page')}">Вернуться на главную</a>
-                            </section>
-                        </body>
-                    </html>"""
+        return render_template('request_take.html', take_good=donation['name'])
     else:
-        return f"""<html>
-                    <body>
-                        <section>
-                            <h2>Подойдите к окну</h2>
-                                <p>Сейчас на складе ничего нет</p>
-                                <a href="{url_for('main_page')}">Вернуться на главную</a>
-                        </section>
-                    </body>
-                </html>"""
+        return render_template('request_take.html')
 
 
 if __name__ == '__main__':
